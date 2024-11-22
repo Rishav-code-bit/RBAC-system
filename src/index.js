9794f629665456bb47ce36af949eb6268fc89b44
Rishav <rishavkumar99057@gmail.com>
2024-11-23 00:35:07 +0530

    authentication

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const dbConnect = require("./config/dbConnect");
+const authRoutes = require("./routes/authRoutes");
+
+dbConnect();
 
 const app = express();
 
@@ -7,6 +11,7 @@ const app = express();
 app.use(express.json())
 
 //Routes
+app.use("/api/auth", authRoutes);
 
 
 //Start the server
