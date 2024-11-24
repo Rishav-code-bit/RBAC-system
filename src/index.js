a4aebf130b67dbee1ffc3c397e051a970b53f14c
Rishav <rishavkumar99057@gmail.com>
2024-11-24 18:36:28 +0530

    login and register pages added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const dbConnect = require("./config/dbConnect");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -9,6 +10,11 @@ dbConnect();
 const app = express();
 
 //Middleware
+app.use(cors({
+    origin: "http://localhost:3000", // Allow requests from your React frontend
+    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
+    credentials: true               // Allow cookies if needed
+}));
 app.use(express.json())
 
 //Routes
